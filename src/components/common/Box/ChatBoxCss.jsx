fcbe1e7d756edcd713352cad264f70d20906d6d7
gka0903 <ham3252@naver.com>
2024-11-20 00:10:56 +0900

    feat: unityPage + css

--- src/components/common/Box/ChatBoxCss.jsx
+++ src/components/common/Box/ChatBoxCss.jsx
@@ -5,7 +5,7 @@ export const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
-  width: 60%;
+  width: 54%;
   height: 80%;
   background-color: ${(props) => props.theme.backgroundColor}; /* 전체 배경 색상 */
   padding: 20px;
--- src/components/common/Box/UnityBox.jsx
+++ src/components/common/Box/UnityBox.jsx
@@ -5,24 +5,14 @@ import {
     HomeButton,
     NavigationBar,
     Wrapper,
-    Header,
     InteractionButton
-} from "./BoxCss.jsx";
+} from "./ChatBoxCss.jsx";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faHome } from '@fortawesome/free-solid-svg-icons';
-import {Unity, useUnityContext} from "react-unity-webgl";
+import FrierenLoader from "../../Unity/FrierenLoader.jsx";
 
 
 export const UnityBox = ({ theme, onClose, name }) => {
-
-    const { unityProvider } = useUnityContext({
-        loaderUrl: "/importInteraction/buildForfrieren/Build/buildForfrieren.loader.js", // Loader 파일 경로
-        dataUrl: "/importInteraction/buildForfrieren/Build/buildForfrieren.data",       // Data 파일 경로
-        frameworkUrl: "/importInteraction/buildForfrieren/Build/buildForfrieren.framework.js", // Framework 파일 경로
-        codeUrl: "/importInteraction/buildForfrieren/Build/buildForfrieren.wasm",       // WebAssembly 파일 경로
-    });
-
-
     return (
         <Wrapper theme={theme}>
             {/* 바깥에 전체를 감싸는 컴포넌트 */}
@@ -41,12 +31,7 @@ export const UnityBox = ({ theme, onClose, name }) => {
             </NavigationBar>
             {/* ChatContainer가 들어가는 영역 */}
             <ChatContainer theme={theme}>
-                <Header theme={theme}>{`HELLO, ${theme.name.toUpperCase()}`}
-                </Header>
-                <Unity
-                    unityProvider={unityProvider}
-                    style={{ width: "100%", height: "100vh", border: "2px solid black" }}
-                />
+                <FrierenLoader/>
             </ChatContainer>
             <NavigationBar theme={theme}>
                 <HomeButton theme={theme} onClick={onClose}>
