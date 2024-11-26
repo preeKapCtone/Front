087579987c626fa238b2a00e0cbcecb414dd685b
root <root@ip-172-31-4-129.ap-northeast-2.compute.internal>
2024-11-26 22:20:57 +0900

    add:proxy

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()],	
   server: {
+    host : true,
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
