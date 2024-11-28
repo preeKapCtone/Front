deb513b6705918ab349c9b0d3b2958817602fe30
Kyu hyunSung <dmp100@naver.com>
2024-11-29 04:50:14 +0900

    Revert "Update README.md"
    This reverts commit 8eb91148823c5b359e2f4821d2c7be73b55b0ba7.

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
-      '/fastapi': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-      },
-    },
-  },
-  // 이미지 등 정적 파일 처리를 위한 설정 추가
-  build: {
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.')[1];
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'img';
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
-        },
-      },
-    },
-  },
-  // 절대 경로 alias 설정
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-    },
-  },
-});
+})
